795266d473b945dcbfbbb5b3cb1c12ed401f25d4
David Cox-Espenlaub <daveespionage@gmail.com>
2023-04-21 20:18:39 +0000

    placeholder js comments

---
+++ src/script.js
@@ -0,0 +1,26 @@
+/**
+ * Class walkthrough
+ * 
+ * Tues
+ * - window onload
+ *   - console.log() when ready
+ * - DOM manipulation
+ *   - onchange
+ * Fri
+ * - DOM manipulation
+ *   - onsubmit
+ *     - log value of field
+ *   - DOM parent-child relationship and navigating using JS
+ *   - append div
+ *   - clear field
+ */
+
+/**
+ * Project requirements
+ * 
+ * - Load script when window is ready
+ * - use onchange to detect when to enable or disable submit button, only if text is present (no empty values!)
+ * - use onsubmit to update chat history
+ * - append a div with the new chat as a child of the chat history container
+ * 
+ *  */ 
